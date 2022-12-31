7aadba82bad43855c078ad0bdda9a3b8e09d6051
Kevin Matthews <kvnrmatthews@gmail.com>
2022-12-30 22:25:45 -0800

    Add components to index file for export

---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+import Link, { CodeLink, HomeLink } from "./components/Link";
+import WithHeavyFooter, {
+  Body,
+  HeavyFooter,
+} from "./components/WithHeavyFooter";
+
+export { WithHeavyFooter, Body, HeavyFooter, Link, CodeLink, HomeLink };
